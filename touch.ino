287a361257962bd2e30d65b35ae8b66219ac5721
Roz Stengle <rosalind.stengle@gmail.com>
2017-04-08 15:06:42 -0500

    arduino alerter

---
+++ touch.ino
@@ -0,0 +1,28 @@
+// Capacitive Touch Sensor Tutorial
+ 
+// When Sig Output is high, touch sensor is being pressed
+#define ctsPin 2// Pin for capactitive touch sensor
+ 
+int LED = 13;
+
+ 
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);  
+  pinMode(ctsPin, INPUT);
+}
+ 
+void loop() {
+  int ctsValue = digitalRead(ctsPin);
+  if (ctsValue == HIGH){
+    digitalWrite(LED, HIGH);
+    Serial.println("TOUCHED");
+  }
+  else{
+    digitalWrite(LED,LOW);
+    Serial.println("not touched");
+  } 
+  delay(500);
+  
+}
+
